1d3508b00e8e4bab5b82105060188d729e8d73c0
Bidyapati Pradhan <32017566+bidyapati@users.noreply.github.com>
2020-12-07 08:11:50 +0530

    Update TopRated.java

--- src/com/edureka/project/youtube/TopRated.java
+++ src/com/edureka/project/youtube/TopRated.java
@@ -18,15 +18,19 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 //Find out the top 10 rated videos.
 public class TopRated {
-	public static class MyMapper extends Mapper<LongWritable,Text, DoubleWritable, Text> {		//data structure to store category and count
-		@Override
+	public static class MyMapper extends Mapper<LongWritable, Text, DoubleWritable, Text> {
+
+	// total rating is rating x rating count
+	@Override
         protected void map(LongWritable key, Text value, Context context)
             throws java.io.IOException, InterruptedException {	    	  
             try{
-               String[] str = value.toString().split("\t");	 
-               String vdoId = str[Utils.YOUTUBE_ID];
-               Double rating = Double.parseDouble(str[Utils.YOUTUBE_RATING]);
-               context.write(new DoubleWritable(rating), new Text(vdoId));
+                String[] str = value.toString().split("\t");	 
+                String vdoId = str[Utils.YOUTUBE_ID];
+                Double rating = Double.parseDouble(str[Utils.YOUTUBE_RATING]);
+		Long ratingCount = Long.parseLong(str[Utils.YOUTUBE_RATING_COUNT]);
+		Double totalRating = rating * ratingCount;
+		context.write(new DoubleWritable(totalRating), new Text(vdoId));
             }
             catch(Exception e)
             {
@@ -47,10 +51,10 @@ public class TopRated {
 		}
 		
 		@Override
-	    public void reduce(DoubleWritable rating, Iterable<Text> vdoIDs,Context context) throws IOException, InterruptedException {
-	       for (Text vdo : vdoIDs) {   
-	    	   rateVdoMap.put(rating.get(), vdo.toString());
-	       }
+	       public void reduce(DoubleWritable rating, Iterable<Text> vdoIDs,Context context) throws IOException, InterruptedException {
+	           for (Text vdo : vdoIDs) {   
+	    	       rateVdoMap.put(rating.get(), vdo.toString());
+	           }
 	       
 		}
 		
@@ -70,22 +74,22 @@ public class TopRated {
 	//hadoop fs -mkdir youtube
 	//hadoop fs -put youtubedata.txt youtube
 	//hadoop jar /mnt/home/edureka_1270998/jars/edurekaProjects.jar com.edureka.project.youtube.TopRated youtube/youtubedata.txt youtube/youtube_top_rated_out
-    public static void main(String[] args) 
+        public static void main(String[] args) 
             throws IOException, ClassNotFoundException, InterruptedException {
 
-			Configuration conf = new Configuration();
-			conf.set("mapreduce.output.textoutputformat.separator", " ");
-			Job job = Job.getInstance(conf);
+		    Configuration conf = new Configuration();
+		    conf.set("mapreduce.output.textoutputformat.separator", " ");
+		    Job job = Job.getInstance(conf);
 		    job.setJarByClass(TopCategories.class);
 		    job.setJobName("Top 10 rated vdo");
 		    job.setMapperClass(MyMapper.class);
 		    job.setNumReduceTasks(1);
 		    job.setReducerClass(MyReducer.class);
-			job.setCombinerClass(MyReducer.class);
+		    // job.setCombinerClass(MyReducer.class);
 		    job.setMapOutputKeyClass(DoubleWritable.class);
 		    job.setMapOutputValueClass(Text.class);
-			job.setOutputKeyClass(DoubleWritable.class);
-			job.setOutputValueClass(Text.class);
+		    job.setOutputKeyClass(DoubleWritable.class);
+		    job.setOutputValueClass(Text.class);
 		  
 		    FileInputFormat.addInputPath(job, new Path(args[0]));
 		    FileOutputFormat.setOutputPath(job, new Path(args[1]));
